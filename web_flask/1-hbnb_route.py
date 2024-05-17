b3433031c94ebe663883bbc4c1d415068b202baf
Obi Chukwu <kennetho.onu@gmail.com>
2024-05-17 15:12:42 +0100

    added a new end point to flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''A Python Module'''
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''displays Hello HBNB!'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''displays HBNB'''
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
